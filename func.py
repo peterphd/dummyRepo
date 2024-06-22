a62a45a66319942e0e8c9f7e7f68a205e4370636
peterphd <peterphdadamu@gmail.com>
2024-06-22 14:31:35 +0100

    function anatomy

---
+++ func.py
@@ -0,0 +1,7 @@
+#This file is a dummy file demo for the anatomy functions in python
+
+def function_name(parameter1, parameter2):
+    #Block of code to be 
+    #executed when function is called
+
+   # return statement 
